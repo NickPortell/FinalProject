32f3e9683d5dfd6b944357aab0dec48eae017631
jeff_ducat <jducat81@gmail.com>
2019-06-18 13:35:11 -0400

    changes to SuperName variable

--- FinalProject/Models/AccountViewModels.cs
+++ FinalProject/Models/AccountViewModels.cs
@@ -64,10 +64,7 @@ namespace FinalProject.Models
 
     public class RegisterViewModel
     {
-        [Required]
-        [Display(Name = "UserName")]
-        public string UserName { get; set; }
-
+     
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
