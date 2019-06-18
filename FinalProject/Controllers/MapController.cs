4ac805b4c237185e3ba537251a7666f3a8b0b6f5
Nick <nick.portell12@gmail.com>
2019-06-18 09:17:35 -0400

    Created MapController

---
+++ FinalProject/Controllers/MapController.cs
@@ -0,0 +1,22 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class MapController : Controller
+    {
+        private franchiseDbEntities ORM = new franchiseDbEntities();
+
+        public ActionResult Index()
+        {
+            List<Crime> list = ORM.Crimes.ToList();
+            return View(list);
+        }
+
+        
+    }
+}
