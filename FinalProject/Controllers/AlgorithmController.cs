721e125556373a2f8e51ececfb1a54b176b4bc7b
Nick <nick.portell12@gmail.com>
2019-06-23 10:10:06 -0400

    Fleshed out algorithm for both good and bad users

--- FinalProject/Controllers/AlgorithmController.cs
+++ FinalProject/Controllers/AlgorithmController.cs
@@ -20,12 +20,10 @@ namespace FinalProject.Controllers
 
             if (user.C_Hero_Villain_ == true)
             {
+                #region Generate a List of crime names that the chosen Ability by the User is 'Good' at or has a 'True' relationship with
                 Ability ability = user.Ability;
 
                 List<string> GoodAt = new List<string>();
-                List<Item> SuggestedItems = new List<Item>();
-                int maxValue = 0;
-
 
                 foreach (var crime in ability.GetType().GetProperties())
                 {
@@ -39,8 +37,11 @@ namespace FinalProject.Controllers
                         }
                     }
                 }
+                #endregion
 
-
+                #region Using that List, find the max crime rate out of the entire Crimes table for only our crime list and get the State Information, img, and suggested items
+                List<Item> SuggestedItems = new List<Item>();
+                int maxValue = 0;
                 foreach (Crime crime in ORM.Crimes)
                 {
                     foreach (var property in crime.GetType().GetProperties())
@@ -65,6 +66,9 @@ namespace FinalProject.Controllers
                 ViewBag.Ability = ability.Ability1;
                 ViewBag.SuggestedItems = SuggestedItems.Distinct();
 
+
+                #endregion
+
                 #region User chooses personality, what mentors they are good with
 
                 List<Mentor> mentors = ORM.Mentors.ToList();
@@ -80,12 +84,73 @@ namespace FinalProject.Controllers
 
 
                 #endregion
-
-
             }
             else
             {
+                #region Generate a List of crime names that the chosen Ability by the User is 'Bad' at or has a 'false' relationship with
+                Ability ability = user.Ability;
 
+                List<string> BadAt = new List<string>();
+
+                foreach (var crime in ability.GetType().GetProperties())
+                {
+                    if (crime.GetValue(ability) is bool)
+                    {
+                        bool b = (bool)crime.GetValue(ability);
+
+                        if (!b)
+                        {
+                            BadAt.Add(crime.Name);
+                        }
+                    }
+                }
+                #endregion
+
+                #region Using that List, find the max crime rate out of the entire Crimes table for only our crime list and get the State Information, img, and suggested items
+                List<Item> SuggestedItems = new List<Item>();
+                int maxValue = 0;
+                foreach (Crime crime in ORM.Crimes)
+                {
+                    foreach (var property in crime.GetType().GetProperties())
+                    {
+
+                        if (BadAt.Contains(property.Name))
+                        {
+                            int current = Convert.ToInt32(property.GetValue(crime));
+                            if (current > maxValue)
+                            {
+                                ViewBag.Name = (string)property.Name;
+                                ViewBag.Img = "..\\Pictures\\StateImages\\" + crime.State + ".jpg";
+                                ViewBag.State = crime;
+                                maxValue = current;
+                                SuggestedItems.Add(ORM.Items.Where(i => i.Crime == (string)property.Name && (i.Availability == "bad" || i.Availability == "both")).FirstOrDefault());
+                            }
+                        }
+                    }
+                }
+
+                ViewBag.Max = maxValue;
+                ViewBag.Ability = ability.Ability1;
+                ViewBag.SuggestedItems = SuggestedItems.Distinct();
+
+
+                #endregion
+
+                #region User chooses personality, what mentors they are good with
+
+                List<Mentor> mentors = ORM.Mentors.ToList();
+                List<string> GoodWith = new List<string>();
+
+                foreach (Mentor m in mentors)
+                {
+                    if (user.Personality == m.Personality)
+                    {
+                        GoodWith.Add(m.Name);
+                    }
+                }
+
+
+                #endregion
             }
 
             return View();
