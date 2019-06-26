caea4a2581e93f664c893efebe80586f2714e7c3
Adam <adwhite.agency@gmail.com>
2019-06-26 13:57:03 -0400

    code cleanup

--- FinalProject/Controllers/MapController.cs
+++ FinalProject/Controllers/MapController.cs
@@ -39,17 +39,7 @@ namespace FinalProject.Controllers
             ViewBag.StateNames = stateNames;
 
             ViewBag.State = ORM.Crimes.Find(state);
-
-            //ViewBag.StateName = ViewBag.
-            //    ViewBag.StatePop = 
-            //    ViewBag.StateViolent = 
-            //    ViewBag.StateHomicide =
-                //ViewBag.StateName =
-                //ViewBag.StateName = 
-                //ViewBag.StateName =
-                //ViewBag.StateName =
-                //ViewBag.StateName =
-
+           
             return View("../Map/Index"); 
         }
 
